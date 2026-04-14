21f6350c591500e27dd3fdf52c81fdec77c2dbbf
pecavalc <pecavalc@student.42berlin.de>
2026-04-14 13:12:21 +0200

    Implement read line from line. Next: find, erase, insert.

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,14 +6,17 @@
 /*   By: pecavalc <pecavalc@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 10:25:15 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/04/14 12:48:10 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/04/14 13:11:06 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <fstream>
 #include <iostream>
 
 static int validate_input(int argc, char **argv) {
+  (void)argv;
+  
   if (argc != 4) {
     std::cout << "Invalid number of arguments.\n";
     return 1;
@@ -28,8 +31,16 @@ int main(int argc, char **argv) {
   std::ifstream file(argv[1]);
   if (!file) {
     std::cout << "Could not open file.\n";
+    return 1;
+  }
 
+  std::string line;
+  if (!std::getline(file, line)) {
+    std::cout << "Could not read line from file.\n";
+    return 1;
+  }
 
+  // TODO: find
 
   return 0;
 }
