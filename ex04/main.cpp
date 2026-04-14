af90378bd714d48d00671e9c978bb6baa0688f5a
pecavalc <pecavalc@student.42berlin.de>
2026-04-14 21:39:25 +0200

    Refactor and review formatting

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,63 +6,77 @@
 /*   By: pecavalc <pecavalc@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 10:25:15 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/04/14 16:50:04 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/04/14 18:05:24 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
 #include <fstream>
 #include <iostream>
+#include <string>
 
-int main(int argc, char **argv) {
+static void write_line(const std::string& line, std::ifstream& infile,
+                       std::ofstream& outfile) {
+  outfile << line;
+  if (!infile.eof()) {
+    outfile << '\n';
+  }
+}
+
+static int find_and_replace(const std::string& s1, const std::string& s2,
+                            std::ifstream& infile, std::ofstream& outfile) {
+  std::string line;
+
+  if (s1.empty()) {
+    while (std::getline(infile, line)) {
+      write_line(line, infile, outfile);
+    }
+  }
+  else {
+    std::string::size_type line_pos;
+    while (std::getline(infile, line)) {
+      line_pos = 0;
+      line_pos = line.find(s1, line_pos);
+      while (line_pos != std::string::npos) {
+        line.erase(line_pos, s1.length());
+        line.insert(line_pos, s2);
+        line_pos += s2.length();
+        line_pos = line.find(s1, line_pos);
+      }
+      write_line(line, infile, outfile);
+    }
+  }
+
+  if (!infile.eof()) {
+    std::cerr << "Could not read line from file.\n";
+    return 1;
+  }
+  return 0;
+}
+
+int main(int argc, char** argv) {
   if (argc != 4) {
     std::cerr << "Invalid number of arguments.\n";
     return 1;
-  } 
+  }
 
-  std::ifstream in_file(argv[1]);
-  if (!in_file) {
+  std::ifstream infile(argv[1]);
+  if (!infile) {
     std::cerr << "Could not open file.\n";
     return 1;
   }
 
-  std::string out_filename = std::string(argv[1]) + ".replace";
-  std::ofstream out_file(out_filename.c_str());
-  if (!out_file) {
-    std::cerr << "Could not create " << out_filename << " file.";
+  std::string outfile_name(std::string(argv[1]) + ".replace");
+  std::ofstream outfile(outfile_name.c_str());
+  if (!outfile) {
+    std::cerr << "Could not create " << outfile_name << " file.\n";
     return 1;
   }
 
   std::string s1 = argv[2];
   std::string s2 = argv[3];
-
-  std::string line;
-  std::string::size_type line_pos;
-
-  while (std::getline(in_file, line)) {
-    if (s1.empty()) {
-      out_file << line + '\n';
-      continue;
-    }
-    line_pos = 0;
-    while(true) {
-      line_pos = line.find(s1, line_pos);
-      if (line_pos != std::string::npos) {
-        line.erase(line_pos, s1.length());
-        line.insert(line_pos, s2);
-        line_pos += s2.length();
-      }
-      else {
-        out_file << line + '\n';
-        break ;
-      }
-    }
-  }
-
-  if (in_file.eof())
-    return 0;
-  else {
-    std::cerr << "Could not read line from file.\n";
+  if (find_and_replace(s1, s2, infile, outfile)) {
     return 1;
   }
+
+  return 0;
 }
